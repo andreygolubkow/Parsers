2e66679719e48d5665399c434797d626f2c0015e
Andrey Golubkow <Andreygolubkow@gmailmail.com>
2018-11-28 11:23:46 +0700

    Сохранил

--- Parsers/GorkoParser/MainForm.cs
+++ Parsers/GorkoParser/MainForm.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using System.Windows.Forms;
+using GorkoRu;
 
 namespace GorkoParser
 {
@@ -21,15 +22,35 @@ namespace GorkoParser
 
         private async void getListButton_Click(object sender, EventArgs e)
         {
+            var restList = new List<Restaurant>();
             HttpClient client = new HttpClient();
-            var url =
-                @"https://api.gorko.ru/api/v2/directory/venues/?fields=metro%2Cparams%2Cpanorama_url%2Ccovers%2Crooms_count_text%2Caddress&city_id=3538&per_page=100&page=1";
-            var data = await client.GetStringAsync(url);
-            
+            var priceCat = "3";
+            var cityId = "3538";
+            var firstUrl =
+                @"https://api.gorko.ru/api/v2/directory/venues/?fields=metro%2Cparams%2Cpanorama_" +
+                "url%2Ccovers%2Crooms_count_text%2Caddress&city_id=" + cityId + "&per_page=100&page=1&price="+priceCat;
+            var firstData = await client.GetStringAsync(firstUrl);
+            var meta = Welcome.FromJson(firstData).Meta;
 
-            textBox1.Text = HttpUtility.UrlDecode(data.Replace(@"\u00", "%"), Encoding.UTF8); 
-            
+            for (int i = 1; i <= meta.PagesCount; i++)
+            {
+                var url =
+                    @"https://api.gorko.ru/api/v2/directory/venues/?fields=metro%2Cparams%2Cpanorama_"+
+                    "url%2Ccovers%2Crooms_count_text%2Caddress&city_id="+cityId+"&per_page=100&page="+i + "&price=" + priceCat;
+                var data = await client.GetStringAsync(url);
 
+                var welcome = Welcome.FromJson(data);
+                restList.AddRange(welcome.Restaurants);
+                
+            }
+            //URL для одного элемента
+            var restUrl =
+                @"https://api.gorko.ru/api/v2/restaurants/25471?embed=rooms&fields=address,params,covers,panorama_matterport_url";
+            foreach (var restaurant in restList)
+            {
+                textBox1.Text += restaurant.Type.Plural == Plural.БанкетныеЗалы;
+            }
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
---
+++ Parsers/GorkoParser/RestaurantData.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Specialized;
+using System.Windows.Forms.VisualStyles;
+using GorkoRu;
+
+namespace GorkoParser
+{
+    /// <summary>
+    /// Схема, которую нужно вывести в Excel.
+    /// </summary>
+    public class RestaurantData
+    {
+        /// <summary>
+        /// Средний чек.
+        /// </summary>
+        public double AvgSum { get; set; }
+
+        /// <summary>
+        /// Количество залов.
+        /// </summary>
+        public int RoomsCount { get; set; }
+
+        /// <summary>
+        /// Название банкетного зала.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// При отеле.
+        /// </summary>
+        public bool AtHotel { get; set; } = false;
+
+        /// <summary>
+        /// База отдыха.
+        /// </summary>
+        public bool IsRecreation { get; set; } = false;
+
+        /// <summary>
+        /// Ресторан.
+        /// </summary>
+        public bool Restaurant { get; set; } = false;
+
+        /// <summary>
+        /// Банкетный зал.
+        /// </summary>
+        public bool Banquet { get; set; } = false;
+
+        /// <summary>
+        /// Другие.
+        /// </summary>
+        public bool Other { get; set; } = false;
+
+        /// <summary>
+        /// В городе или за городом(false).
+        /// </summary>
+        public bool InCity { get; set; } = true;
+
+        /// <summary>
+        /// Адрес.
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Телефон.
+        /// </summary>
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// Instagram.
+        /// </summary>
+        public string Instagram { get; set; }
+
+        /// <summary>
+        /// Другие ссылки.
+        /// </summary>
+        public string OtherLinks { get; set; }
+
+        /// <summary>
+        /// Вместимость.
+        /// </summary>
+        public int Capacity { get; set; }
+
+        public static RestaurantData FromRestaurant(Restaurant restaurant)
+        {
+            var restData = new RestaurantData();
+            restData.RoomsCount = restaurant.Rooms.Count;
+            restData.Name = restaurant.Name;
+            //restData.AtHotel = restaurant.Type.Name
+            return restData;
+        }
+    }
+}
